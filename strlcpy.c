4227ec6dd3ab3b03b6af246921e6e3638de9ddbf
Reem Eldahlish <reem.essam.eldin@gmail.com>
2023-11-17 14:17:05 +0100

    strlcpy

---
+++ strlcpy.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int ft_strlcpy(char *dest, char *src, int n)
+{
+    int i;
+    int j;
+
+    i = 0;
+    j = 0;
+    while(dest[i] != '\0' && dest[i] <= n)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    while (src[j] != '\0')
+    {
+        j++;
+    }
+    return(j);
+}
+
+int main()
+{
+    char s[] = "Hello";
+    char d[] = "ReemEldahlish";
+    printf("The source is: %s\n", s);
+    printf("The destination is: %s\n", d);
+    printf("\nThe result is: %lu\n", strlcpy(d, s, 4));
+    printf("The source is: %s\n", s);
+    printf("The destination is: %s\n", d);
+    printf("\nMY FUNCTION The result is: %d\n", ft_strlcpy(d, s, 4));
+    printf("MY FUNCTION The source is: %s\n", s);
+    printf("MY FUNCTION The destination is: %s\n", d);
+}
