b4398a62d7f65da297d731bf2fcbfe3b0a98e453
Reem Eldahlish <reem.essam.eldin@gmail.com>
2023-11-20 14:27:09 +0100

    memset

---
+++ ft_memset.c
@@ -0,0 +1,12 @@
+void *ft_memset(void *str, int c, size_t n)
+{
+    unsigned char *tmp_ptr;
+    tmp_ptr = (unsigned char *) str;
+    
+    while (n > 0)
+    {
+        *(tmp_ptr++) = (unsigned char) c;
+        n--;
+    }
+    return(str);
+}
